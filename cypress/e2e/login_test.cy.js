b0046c25c0187a19047951f7e47ec3c00fe6b501
vedpull <vedpulat@gmail.com>
2025-12-11 17:13:46 +0300

    Hardcoded veriler kaldırıldı, fixtures yapısınan geçildi

--- cypress/e2e/login_test.cy.js
+++ cypress/e2e/login_test.cy.js
@@ -3,24 +3,41 @@ import LoginPage from "../pages/LoginPage"
 
 describe('Login Testleri', ()=>{
 
+    let userData;
+
+    before(()=>{
+        cy.fixture('users').then((data)=>{
+            userData = data;
+        })
+    })
+
     beforeEach(()=>{
         cy.visit('https://www.saucedemo.com/')
     })
 
     it ('Başarılı Giriş', ()=>{
 
-        LoginPage.typeUsername('standard_user')
-        LoginPage.typePassword('secret_sauce')
+        LoginPage.typeUsername(userData.validUser.username)
+        LoginPage.typePassword(userData.validUser.password)
         LoginPage.clickLogin()
 
         cy.url().should('include', '/inventory.html')
     })
 
     it ('Hatalı Giriş', ()=>{
-        LoginPage.typeUsername('standard_user')
-        LoginPage.typePassword('yanlis_sifre_123')
+        LoginPage.typeUsername(userData.invalidUser.username)
+        LoginPage.typePassword(userData.invalidUser.password)
         LoginPage.clickLogin()
 
         LoginPage.errorMessage.should('have.text', 'Epic sadface: Username and password do not match any user in this service')
     })
+
+    it ('Kilitli Kullanıcı Girişi', ()=>{
+        LoginPage.typeUsername(userData.lockedUser.username)
+        LoginPage.typePassword(userData.lockedUser.password)
+        LoginPage.clickLogin()
+
+        LoginPage.errorMessage.should('have.text', 'Epic sadface: Sorry, this user has been locked out.')
+    })
+    
 })
